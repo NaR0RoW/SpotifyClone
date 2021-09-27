b3e8ee481a5dc44f218d1fa5df6fe51a4291e272
NaR0RoW <nar0row@gmail.com>
2021-09-27 21:43:12 +0300

    Setup project

---
+++ SpotifyClone/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Home"
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ SpotifyClone/Controllers/Other/PlayerViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
