39ac6da493ae54e2e3451e00fbb8ad9c2d9fd8c8
NaR0RoW <nar0row@gmail.com>
2021-09-27 23:21:14 +0300

    Create webView & WelcomeViewController

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -4,10 +4,24 @@ final class AuthManager {
     
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "1b309054d722402dbf7bd0c0e43f8dbb"
+        static let clientSecret = "7e9517b6eb63446a9c64059bb8ca7fa8"
+    }
+    
     private init() {}
     
+    public var signInURL: URL? {
+        let base = "https://accounts.spotify.com/authorize"
+        let scopes = "user-read-private"
+        let redirectURI = "https://github.com/NaR0RoW/SpotifyClone"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
+    
     var isSignedIn: Bool {
-        return true
+        return false
     }
     
     private var accessToken: String? {
@@ -24,5 +38,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        // Get Token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
