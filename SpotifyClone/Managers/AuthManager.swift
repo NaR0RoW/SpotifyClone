f06c2e2512744b089fb7132283385cb4e41f0c29
NaR0RoW <nar0row@gmail.com>
2021-09-29 14:11:15 +0300

    Exchange code for token added & need to fix refreshIfNeeded method

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -5,8 +5,9 @@ final class AuthManager {
     static let shared = AuthManager()
     
     struct Constants {
-        static let clientID = "1b309054d722402dbf7bd0c0e43f8dbb"
-        static let clientSecret = "7e9517b6eb63446a9c64059bb8ca7fa8"
+        static let clientID = ""
+        static let clientSecret = ""
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     
     private init() {}
@@ -21,34 +22,116 @@ final class AuthManager {
     }
     
     var isSignedIn: Bool {
-        return false
+        return accessToken != nil
     }
     
     private var accessToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "access_token")
     }
     
     private var refreshToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "refresh_token")
     }
     
     private var tokenExpirationDate: Date? {
-        return nil
+        return UserDefaults.standard.object(forKey: "expirationDate") as? Date
     }
     
     private var shouldRefreshToken: Bool {
-        return false
+        guard let expirationDate = tokenExpirationDate else { return false }
+        let currentDate = Date()
+        let fiveMinutes: TimeInterval = 300
+        
+        return currentDate.addingTimeInterval(fiveMinutes) >= expirationDate
     }
     
     public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
         // Get Token
+        guard let url = URL(string: Constants.tokenAPIURL) else { return }
+        
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "redirect_uri", value: "https://github.com/NaR0RoW/SpotifyClone")
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        let basicToken = Constants.clientID + ":" + Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else { print("Failure to get base64"); completion(false); return }
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                self?.cacheToken(result: result)
+                completion(true)
+            } catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
     }
     
-    public func refreshAccessToken() {
+    public func refreshIfNeeded(completion: @escaping ((Bool) -> Void)) {
+//                guard shouldRefreshToken else {
+//                    completion(true)
+//                    return
+//                }
         
+        // FIXME: why return?; refreshToken == nil
+        guard let refreshToken = self.refreshToken else {
+            print(String(describing: refreshToken))
+            return
+        }
+        // Refresh the token
+        guard let url = URL(string: Constants.tokenAPIURL) else { return }
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "refresh_token"),
+            URLQueryItem(name: "refresh_token", value: refreshToken)
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        let basicToken = Constants.clientID + ":" + Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else { print("Failure to get base64"); completion(false); return }
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else { completion(false); return }
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                print("Successfully refreshed")
+                self?.cacheToken(result: result)
+                completion(true)
+            } catch {
+                // FIXME: The data couldn’t be read because it is missing. But data = 80 bytes
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
     }
     
-    private func cacheToken() {
-        
+    private func cacheToken(result: AuthResponse) {
+        UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
+        if let refresh_token = result.refresh_token {
+            UserDefaults.standard.setValue(refresh_token, forKey: "refresh_token")
+        }
+        UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
     }
 }
+
--- SpotifyClone/Resources/AppDelegate.swift
+++ SpotifyClone/Resources/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window.makeKeyAndVisible()
         self.window = window
+        
+//        AuthManager.shared.refreshIfNeeded { success in
+//            print(success)
+//        }
 
         return true
     }
